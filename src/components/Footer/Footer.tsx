a742d8574d6fb57f5f33b56291a14684487ccd99
Diana Caldeira <dianamila07@hotmail.com>
2024-07-03 13:42:09 -0300

    css + crm

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Footer.css';
+import { FaInstagram } from 'react-icons/fa';
 
 const Footer: React.FC = () => (
-  <footer>
-    <p>Neuropediatra Laura T</p>
+  <footer className="footer">
+    <p className="footer__text">&copy; 2024 Neuropediatra Laura T. Todos os direitos reservados.</p>
+    <p className="footer__text">CRM: 78673</p>
+    <div className="footer-social">
+      <a href="https://www.instagram.com/seu_instagram" target="_blank" rel="noopener noreferrer">
+        <FaInstagram size={24} />
+      </a>
+    </div>
   </footer>
 );
 

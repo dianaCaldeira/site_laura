a898b6fb9ce65e416ba7044177bf6a0021d0ab23
Diana Caldeira <dianamila07@hotmail.com>
2024-07-04 15:27:20 -0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './Footer.css';
-import { FaInstagram } from 'react-icons/fa';
+import { FaInstagram, FaFacebookF, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
+import Logo2 from '../../assets/Logo02.jpeg'
 
 const Footer: React.FC = () => (
   <footer className="footer">
-    <p className="footer__text">&copy; 2024 Neuropediatra Laura T. Todos os direitos reservados.</p>
-    <p className="footer__text">CRM: 78673</p>
-    <div className="footer-social">
-      <a href="https://www.instagram.com/lauraneuroped/" target="_blank" rel="noopener noreferrer">
-        <FaInstagram size={24} />
+    <div className="footer__left">
+      <img src={Logo2} alt="Logo2" className="footer__logo" />
+    </div>
+    <div className="footer__center">
+      <p className="footer__text">&copy; 2024 Neuropediatra Laura T. Todos os direitos reservados.</p>
+      <p className="footer__text">CRM: 78673</p>
+    </div>
+    <div className="footer__right">
+      <a href="https://www.instagram.com/lauraneuroped/" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+        <FaInstagram size={28} />
+      </a>
+      <a href="https://www.facebook.com/lauraneuroped" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+        <FaFacebookF size={28} />
+      </a>
+      <a href="https://www.linkedin.com/in/lauraneuroped" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+        <FaLinkedinIn size={28} />
+      </a>
+      <a href="https://www.youtube.com/channel/lauraneuroped" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+        <FaYoutube size={28} />
       </a>
     </div>
   </footer>

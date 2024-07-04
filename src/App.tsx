89221429ecb1b0ae6630e261b35df91aef46f478
Diana Caldeira <dianamila07@hotmail.com>
2024-07-04 15:59:49 -0300

    whats flout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { FaWhatsapp } from 'react-icons/fa';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/HomePage';
@@ -9,6 +10,8 @@ import ContactPage from './pages/Contact/ContactPage';
 import './styles/global.css';
 
 const App: React.FC = () => {
+  const whatsappLink = "https://wa.me/5531995626630";
+
   return (
     <Router>
       <div className="app">
@@ -22,6 +25,9 @@ const App: React.FC = () => {
           </Routes>
         </main>
         <Footer />
+        <a href={whatsappLink} className="whatsapp-float" target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp />
+        </a>
       </div>
     </Router>
   );

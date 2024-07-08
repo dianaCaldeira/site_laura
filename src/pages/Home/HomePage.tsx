8994434972fdaa7c0f9efac4de03092be86b22bf
Diana Caldeira <dianamila07@hotmail.com>
2024-07-08 16:32:28 -0300

    ajustes

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -5,12 +5,13 @@ import lauraWebp from '../../assets/optimized/Laura01.webp';
 import lauraJpg from '../../assets/optimized/Laura01.jpg';
 import laura02Webp from '../../assets/optimized/Laura02.webp';
 import laura02Jpg from '../../assets/optimized/Laura02.jpg';
+import backgroundImage from '../../assets/backgroud-image.jpg';
 
 const Home: React.FC = () => {
   const whatsappLink = "https://wa.me/5531995626630";
 
   return (
-    <div className="home-page">
+    <div className="home-page" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
       <h1 className="welcome-heading">SEJAM BEM-VINDOS</h1>
       <div className="home-content">
         <div className="home-image">
@@ -79,22 +80,9 @@ const Home: React.FC = () => {
       <div className="faq-container">
         <h2>Perguntas Frequentes</h2>
         <div className="faq-topics">
-          <FAQTopic
-            question="O que devo levar em uma primeira consulta com o Neuropediatra?"
-            answer="Na primeira consulta não se esqueça de levar a caderneta da criança e a alta da maternidade. Se possível, leve exames prévios que já foram realizados e sumários de alta de internações hospitalares. Em caso de queixas escolares, não se esqueça de levar relatório atualizado da escola! Se a criança for acompanhada por algum outro profissional, como psicólogos, terapeutas ocupacionais e fonoaudiólogos, relatórios destes profissionais são sempre bem-vindos."
-          />
-          <FAQTopic
-            question='Meu filho está atrasado ou "cada criança tem seu tempo"?'
-            answer="Muito cuidado ao aguardar 'o tempo' de cada criança para atingir os marcos esperados no desenvolvimento neuropsicomotor. Qualquer dúvida em relação a marcos deve levar à busca de profissional capacitado o mais rapidamente possível! A intervenção precoce é a melhor forma de tratamento para atrasos do desenvolvimento e não podemos perder tempo para iniciar a estimulação."
-          />
-          <FAQTopic
-            question="O que fazer quando a criança apresenta problemas para dormir?"
-            answer="Queixas relacionadas ao sono são muito recorrentes no dia a dia do Neurologista Infantil. O sono é indispensável para o crescimento e desenvolvimento adequados da criança, e problemas no mesmo devem levar à busca de ajuda especializada. Para o dia da consulta, lembre-se de detalhar toda a rotina da criança, bem como os hábitos da casa. Alterações de sono, em geral, dizem respeito à saúde integral e aos hábitos da criança e, muitas vezes, podem ser corrigidas apenas com mudanças no dia a dia da criança e da família."
-          />
-          <FAQTopic
-            question="Como saber se meu filho tem TDAH?"
-            answer="O diagnóstico de TDAH é confiável e válido quando realizado por profissional capacitado e seguindo os critérios estabelecidos na literatura. Hoje, o diagnóstico é feito por meio de entrevista clínica com a família buscando avaliar os critérios de TDAH do Manual Diagnóstico de Transtornos Mentais. Não existem exames para diagnóstico. Em caso de suspeita, leve seu filho a profissional capaz de realizar o diagnóstico, como um neurologista infantil ou psiquiatra. Diagnosticar e tratar corretamente o TDAH é de extrema importância, uma vez que o comprometimento funcional causado pelo transtorno acarreta em piora da qualidade de vida do paciente."
-          />
+          {faqData.map((faq, index) => (
+            <FAQTopic key={index} question={faq.question} answer={faq.answer} />
+          ))}
         </div>
         <div className="button-container">
           <Button
@@ -113,5 +101,24 @@ const FAQTopic: React.FC<{ question: string; answer: string }> = ({ question, an
     <p>{answer}</p>
   </div>
 );
+
+const faqData = [
+  {
+    question: "O que devo levar em uma primeira consulta com o Neuropediatra?",
+    answer: "Na primeira consulta não se esqueça de levar a caderneta da criança e a alta da maternidade. Se possível, leve exames prévios que já foram realizados e sumários de alta de internações hospitalares. Em caso de queixas escolares, não se esqueça de levar relatório atualizado da escola! Se a criança for acompanhada por algum outro profissional, como psicólogos, terapeutas ocupacionais e fonoaudiólogos, relatórios destes profissionais são sempre bem-vindos."
+  },
+  {
+    question: 'Meu filho está atrasado ou "cada criança tem seu tempo"?',
+    answer: "Muito cuidado ao aguardar 'o tempo' de cada criança para atingir os marcos esperados no desenvolvimento neuropsicomotor. Qualquer dúvida em relação a marcos deve levar à busca de profissional capacitado o mais rapidamente possível! A intervenção precoce é a melhor forma de tratamento para atrasos do desenvolvimento e não podemos perder tempo para iniciar a estimulação."
+  },
+  {
+    question: "O que fazer quando a criança apresenta problemas para dormir?",
+    answer: "Queixas relacionadas ao sono são muito recorrentes no dia a dia do Neurologista Infantil. O sono é indispensável para o crescimento e desenvolvimento adequados da criança, e problemas no mesmo devem levar à busca de ajuda especializada. Para o dia da consulta, lembre-se de detalhar toda a rotina da criança, bem como os hábitos da casa. Alterações de sono, em geral, dizem respeito à saúde integral e aos hábitos da criança e, muitas vezes, podem ser corrigidas apenas com mudanças no dia a dia da criança e da família."
+  },
+  {
+    question: "Como saber se meu filho tem TDAH?",
+    answer: "O diagnóstico de TDAH é confiável e válido quando realizado por profissional capacitado e seguindo os critérios estabelecidos na literatura. Hoje, o diagnóstico é feito por meio de entrevista clínica com a família buscando avaliar os critérios de TDAH do Manual Diagnóstico de Transtornos Mentais. Não existem exames para diagnóstico. Em caso de suspeita, leve seu filho a profissional capaz de realizar o diagnóstico, como um neurologista infantil ou psiquiatra. Diagnosticar e tratar corretamente o TDAH é de extrema importância, uma vez que o comprometimento funcional causado pelo transtorno acarreta em piora da qualidade de vida do paciente."
+  }
+];
 
 export default Home;
--- src/scripts/optimize-images.js
+++ src/scripts/optimize-images.js
@@ -6,24 +6,24 @@ const inputDir = path.join(__dirname, '..', 'assets');
 const outputDir = path.join(__dirname, '..', 'assets', 'optimized');
 
 if (!fs.existsSync(outputDir)){
-    fs.mkdirSync(outputDir, { recursive: true });
+  fs.mkdirSync(outputDir, { recursive: true });
 }
 
 fs.readdirSync(inputDir).forEach(file => {
-    if (file.match(/\.(jpg|jpeg|png)$/i)) {
-        const inputPath = path.join(inputDir, file);
-        const outputName = path.parse(file).name;
+  if (file.match(/\.(jpg|jpeg|png)$/i)) {
+    const inputPath = path.join(inputDir, file);
+    const outputName = path.parse(file).name;
 
-        sharp(inputPath)
-            .resize(800) // Ajuste conforme necessário
-            .webp({ quality: 80 })
-            .toFile(path.join(outputDir, `${outputName}.webp`))
-            .then(() => console.log(`Converted ${file} to WebP`));
-        
-        sharp(inputPath)
-            .resize(800) // Ajuste conforme necessário
-            .jpeg({ quality: 80 })
-            .toFile(path.join(outputDir, `${outputName}.jpg`))
-            .then(() => console.log(`Optimized ${file} as JPEG`));
-    }
+    sharp(inputPath)
+      .resize(800)
+      .webp({ quality: 80 })
+      .toFile(path.join(outputDir, `${outputName}.webp`))
+      .then(() => console.log(`Converted ${file} to WebP`));
+    
+    sharp(inputPath)
+      .resize(800) 
+      .jpeg({ quality: 80 })
+      .toFile(path.join(outputDir, `${outputName}.jpg`))
+      .then(() => console.log(`Optimized ${file} as JPEG`));
+  }
 });
